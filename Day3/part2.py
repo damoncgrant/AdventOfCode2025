f816a391a52994e32a3525b1ac93d969f87a9c7b
Damon Grant <dcgrant@ualberta.ca>
2025-12-03 14:29:01 -0700

    Finished Day 3 Part 2

--- Day3/part2.py
+++ Day3/part2.py
@@ -1,6 +1,6 @@
 total = 0
 
-with open("example.txt", "r") as file:
+with open("input.txt", "r") as file:
     banks = file.readlines()
     for bank in range(len(banks)):
         banks[bank] = list(banks[bank].strip())
@@ -8,21 +8,31 @@ with open("example.txt", "r") as file:
         for i in range(len(banks[bank])):
             banks[bank][i] = int(banks[bank][i])
         
+        currentBank = banks[bank]
         stack = []
-        num = ""
-        counter = 12
+        remaining = 12
 
-        for i in range(len(banks[bank])):
-            if counter == 0:
-                num += stack.join("")
-            if stack == [] and counter > 0:
-                stack.append(banks[bank][i])
-                counter -= 1
+        for i in range(len(currentBank)):
+            dist = len(currentBank) - i 
+            
+            if stack == []:
+                stack.append(currentBank[i])
+                remaining -= 1
                 continue
 
+            while currentBank[i] > stack[-1] and dist > remaining:
+                stack.pop()
+                remaining += 1
+
+                if stack == []:
+                    break
+
+            if remaining > 0:
+                stack.append(currentBank[i])
+                remaining -= 1
             
-
-
-
+        num = int("".join([str(i) for i in stack]))
+        total += num
+            
 print(total)
             
